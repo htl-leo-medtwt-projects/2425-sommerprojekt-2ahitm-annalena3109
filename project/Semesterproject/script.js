5a4d0cae5d07e2e4e3f22f2321ed5af5d2e387a6
Annalena <a.see@students.htl-leonding.ac.at>
2025-03-13 13:52:27 +0100

    progress

--- project/Semesterproject/script.js
+++ project/Semesterproject/script.js
@@ -1,9 +1,12 @@
+//variables
+let username
+let body = document.body
 let firstPage = document.getElementById("firstPage")
 let inputPage = document.getElementById("inputPage")
+let spiderPage = document.getElementById("spiderPage")
 
-document.getElementById("infos").style.display = "none"
-let body = document.body
 
+//info button start pages
 function showInfos(){
     document.getElementById("infos").style.display = "block"
     document.getElementById("infoButton").style.display = "none"
@@ -15,8 +18,19 @@ function closeInfos(){
     document.getElementById("startText").style.display = "block"
 }
 
+
+//switch side functions:
 function goToInputPage(){
     firstPage.style.display = "none"
     body.style.backgroundImage = "url(img/inputBG.jpg)"
     inputPage.style.display = "block"
+}
+
+function saveAndStart(){
+    inputPage.style.display = "none"
+    body.style.backgroundImage = "url(img/spiderBG.jpg)"
+    spiderPage.style.display = "block"
+    username = document.getElementById("nameInput").value
+    document.getElementById("nameInput").value = ""
+
 }
